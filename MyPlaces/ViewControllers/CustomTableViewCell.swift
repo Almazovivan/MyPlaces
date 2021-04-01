c50f1a3bed064a019415f48c359a18f3508b7202
Иван Алмазов <silvep4@gmail.com>
2021-04-01 17:15:09 +0300

    Initial Commit

---
+++ MyPlaces/ViewControllers/CustomTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Ivan Almazov on 07/11/2020.
+//  Copyright © 2018 Ivan Almazov. All rights reserved.
+//
+
+import UIKit
+import Cosmos
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView! {
+        didSet {
+            imageOfPlace.layer.cornerRadius = imageOfPlace.frame.size.height / 2
+            imageOfPlace.clipsToBounds = true
+        }
+    }
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var cosmosView: CosmosView! {
+        didSet {
+            cosmosView.settings.updateOnTouch = false
+        }
+    }
+    
+
+}
